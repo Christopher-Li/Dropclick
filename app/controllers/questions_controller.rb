17c0924e8abd34a5ac918d3e3d4d9252c696a938
Christopher-Li <Chrisssss158@gmail.com>
2014-11-16 22:36:26 -0600

    adding continuity across pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,41 +1,32 @@
 class QuestionsController < ApplicationController
 	def new
-		# q = Question.new
-		# q.save
+		q = Question.new
+		q.content = ""
+		q.save
 		redirect_to "/questions/#{ q.id }"
 	end
 
-	def create
-	end
-
 	def show
-		if params["new_answer"]
-			a = Answer.new
-			a.content = params["new_answer"]
-			a.question_id = params["id"]
-			a.save
-		end
 		@answers = Answer.where("question_id" => params["id"])
-		if Question.find_by("id", params["id"])
-			@question = Question.find_by("id", params["id"])
-		end
+		@question = Question.find_by("id" => params["id"])
 	end
 
 	def index
+		@questions = Question.all
 	end
 
 	def edit
 	end
 
 	def update
-		@question = Question.find_by("id", params["id"])
+		@question = Question.find_by("id" => params["id"])
 		@question.content = params["desc"]
 		@question.save
 		@answers = Answer.where("question_id" => params["id"])
 		counter = 0
 		@answers.each do |answer|
-			if params["answer${ counter }"]
-				answer.content = params["answer${ counter }"]
+			if params["answer#{ counter }"] != ""
+				answer.content = params["answer#{ counter }"]
 				answer.save
 			else
 				answer.delete
@@ -46,12 +37,32 @@ class QuestionsController < ApplicationController
 				a = Answer.new
 				a.content = params["new_answer"]
 				a.question_id = params["id"]
+				a.votes = 0
 				a.save
 		end
 		redirect_to "/questions/#{ params["id"] }"
 	end
 
 	def destroy
+		q = Question.find_by_id(params["id"])
+		q.destroy
+		redirect_to "/questions"
+	end
+
+	def answer
+		@answers = Answer.where("question_id" => params["id"])
+		@question = Question.find_by("id" => params["id"])
+	end
+
+	def answersubmit
+		@answers = Answer.where("question_id" => params["id"])
+		@answers[params["Answerchoice"].to_i].votes += 1
+		@answers[params["Answerchoice"].to_i].save
+		redirect_to "/questions/#{params["id"]}/answer"
+	end
+
+	def results
+		@answers = Answer.where("question_id" => params["id"])
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   # CREATE
   get "/new_question" => "questions#new"
-  get "/create_question" => "questions#create"
 
   # READ
-  get "/questions/:id" => "questions#show"
   get "/questions" => "questions#index"
 
   # UPDATE
-  get "/questions/:id/edit" => "questions#edit"
+  get "/questions/:id" => "questions#show"
   get "/questions/:id/update" => "questions#update"
+
+  # SHOW RESULTS
+  get "/questions/:id/results" => "questions#results"
 
   # DESTROY
   get "/questions/:id/destroy" => "questions#destroy"
+
+  # Answer
+  get "/questions/:id/answer" => "questions#answer"
+  get "/questions/:id/answersubmit" => "questions#answersubmit"
 end
